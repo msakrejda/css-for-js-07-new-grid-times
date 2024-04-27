fa853aa3eef1d94a1391ee3d019c4457bac5918c
Maciek Sakrejda <m.sakrejda@gmail.com>
2024-04-26 18:02:17 -0700

    Exercise #1

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -10,7 +10,7 @@ import Button from '../Button';
 
 const Header = () => {
   return (
-    <header>
+    <Wrapper>
       <SuperHeader>
         <Row>
           <ActionGroup>
@@ -31,17 +31,47 @@ const Header = () => {
       <MainHeader>
         <Logo />
       </MainHeader>
-    </header>
+      <SubscribeBlurb>
+        <Button>Subscribe</Button>
+        <span>Already a subscriber?</span>
+      </SubscribeBlurb>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled(MaxWidthWrapper)`
+  @media ${QUERIES.laptopAndUp} {
+    display: flex;
+    grid-template-columns: min-content max-content min-content;
+    align-items: center;
+    justify-content: space-between;
+  }
+`;
+
+const SubscribeBlurb = styled.div`
+  display: none;
+  span {
+    text-decoration: underline;
+    font-style: italic;
+  }
+  text-align: center;
+  @media ${QUERIES.laptopAndUp} {
+    display: block;
+  }
+`;
 
 const SuperHeader = styled.div`
   padding: 16px 0;
   background: var(--color-gray-900);
   color: white;
+
+  @media ${QUERIES.laptopAndUp} {
+    background: var(--color-gray-100);
+    color: var(--color-gray-900);
+  }
 `;
 
-const Row = styled(MaxWidthWrapper)`
+const Row = styled.div`
   display: flex;
   justify-content: space-between;
 `;
@@ -49,6 +79,12 @@ const Row = styled(MaxWidthWrapper)`
 const ActionGroup = styled.div`
   display: flex;
   gap: 24px;
+
+  &:last-child {
+    @media ${QUERIES.laptopAndUp} {
+      display: none;
+    }
+  }
 
   /*
     FIX: Remove the inline spacing that comes with
@@ -59,12 +95,17 @@ const ActionGroup = styled.div`
   }
 `;
 
-const MainHeader = styled(MaxWidthWrapper)`
+const MainHeader = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
   margin-top: 32px;
   margin-bottom: 48px;
+
+  @media ${QUERIES.tabletAndUp} {
+    margin-bottom: 64px;
+    justify-content: space-between;
+  }
 `;
 
 export default Header;
